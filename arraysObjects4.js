713af45db0c2fad028cd2e697c2cd3f47357f1f6
Vega8991 <manuelvega011204@gmail.com>
2025-10-08 11:23:01 +0200

    Arrays4 finished

--- arraysObjects4.js
+++ arraysObjects4.js
@@ -36,9 +36,29 @@ const libros = [
 ]; 
 
 // 2. Crea una función para encontrar un libro por su título (ignorando mayúsculas/minúsculas)
+function enctLibrosTitulo(titulo) {
+    return libros.find(libro => libro.titulo.toLowerCase() === titulo.toLowerCase())
+}
 
+const libro1 = enctLibrosTitulo('1984')
+console.log('1 Libro encontrado: ', libro1)
 // 3. Crea una función para encontrar el primer libro de un autor específico
+function encLibroAutor(autor) {
+    return libros.find(libro => libro.autor.toLowerCase() === autor.toLowerCase())
+}
 
+const libro2 = encLibroAutor('Jane Austen')
+console.log('2 Autor encontrado: ', libro2)
 // 4. Crea una función para encontrar un libro que cumpla múltiples criterios (por ejemplo, autor y año)
 
+function encontrarLibroCrit(criterios){
+    return libros.find(libro => {
+        return Object.keys(criterios).every(key => libro[key] === criterios[key])
+    })
+}
+const libro3 = encontrarLibroCrit({autor: 'Jane Austen', año: 1813})
+console.log('3 Libro encontrado: ', libro3)
+
 // 5. Intenta buscar un libro que no exista y muestra el resultado
+const libroNoExiste = encLibroAutor('Autor Falso')
+console.log('4 Búsqueda no existente: ', libroNoExiste)
